17f61dfb96dbd8dd511e194c754d91ba1d0539ea
kindermannhubert <hubert.kindermann@hotmail.com>
2015-08-18 22:20:48 +0200

    Fix #21 (Tests InjectCodeOnExitWithMoreExitsTest and InjectCodeOnExitWithJumpToExit are failing.).

--- ExtensibleILRewriter/Extensions/MethodDefinitionExtensions.cs
+++ ExtensibleILRewriter/Extensions/MethodDefinitionExtensions.cs
@@ -2,6 +2,7 @@
 using Mono.Cecil.Cil;
 using Mono.Cecil.Rocks;
 using Mono.Collections.Generic;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ExtensibleILRewriter.Extensions
@@ -69,14 +70,31 @@ namespace ExtensibleILRewriter.Extensions
 
             instructions.Clear();
 
+            var branchOperandReplacements = new Dictionary<Instruction, Instruction>();
             foreach (var ins in oldInstructions)
             {
                 if (ins.OpCode.Code == Code.Ret)
                 {
                     instructions.AddRange(newInstructions);
+                    branchOperandReplacements.Add(ins, newInstructions[0]); // all branches should go to our new instruction instead of directly going to ret
                 }
 
                 instructions.Add(ins);
+            }
+
+            // repair branching to ret instructions, so the branching is made to begining of injected code
+            foreach (var ins in instructions)
+            {
+                if (ins.OpCode.FlowControl == FlowControl.Branch || ins.OpCode.FlowControl == FlowControl.Cond_Branch)
+                {
+                    var branchOperand = (Instruction)ins.Operand;
+
+                    Instruction newBranchOperand;
+                    if (branchOperandReplacements.TryGetValue(branchOperand, out newBranchOperand))
+                    {
+                        ins.Operand = newBranchOperand;
+                    }
+                }
             }
 
             body.OptimizeMacros();
